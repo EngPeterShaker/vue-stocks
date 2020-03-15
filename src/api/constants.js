677df91659319af9ed7c12f1216a367c4f2e4e7e
peter <peters.shaker@gmail.com>
2020-03-16 01:03:32 +0200

    fetching data by axios and Vuex

---
+++ src/api/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL= 'https://www.alphavantage.co/query'
+export const API_key= 'MV0Z69TU2PCKJQSM'
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {API_key , BASE_URL} from "../api/constants";
+import axios from "axios";
 
 Vue.use(Vuex)
-
+Vue.use(axios)
 export default new Vuex.Store({
   state: {
+    stocksData:{},
+    FxData:{}
   },
   mutations: {
+    SAVE_STOCKS(state, stocksData) {
+      state.stocksData = stocksData;
+    },
+    SAVE_FX(state, FxData) {
+      state.FxData = FxData;
+    }
   },
   actions: {
+    fetchStocks({commit}) {
+      axios.get(BASE_URL, {
+        params: {
+          function:'TIME_SERIES_INTRADAY',
+          symbol:'MSFT',
+          interval:'1min',
+          apikey:API_key
+        }
+      })
+      .then(result => {
+        commit('SAVE_STOCKS', result.data['Time Series (1min)']);
+      }).catch(error => {
+        throw new Error(`API ${error}`);
+      });
+    },
+    fetchFx({commit}) {
+      axios.get(BASE_URL, {
+        params: {
+          function:'FX_INTRADAY',
+          from_symbol:'EUR',
+          to_symbol:'USD',
+          interval:'1min',
+          apikey:API_key
+        }
+      })
+      .then(result => {
+        commit('SAVE_FX', result.data['Time Series FX (1min)']);
+      }).catch(error => {
+        throw new Error(`API ${error}`);
+      });
+    }
   },
   modules: {
   }
