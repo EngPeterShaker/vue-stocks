af492f745df139ea210d75395a44e652ac94cf2c
peter <peters.shaker@gmail.com>
2020-03-16 12:45:50 +0200

    formatting x axis labels

--- src/components/Fx.vue
+++ src/components/Fx.vue
@@ -1,44 +1,43 @@
 <template>
   <div>
     <h1>{{ this.duration }}</h1>
-        <v-container class="grey lighten-5">
-      <v-row  no-gutters>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
         <v-col class="wrapper">
-
-    <v-card
-      v-for="(item, itemKey) in FxData"
-      class="mx-auto my-4 px-4"
-      max-width="344"
-      outlined
-      :key="itemKey"
-    >
-      <v-row align="center">
-        <v-col class="display-3" cols="6">
-          <p class="close_text">{{ item["4. close"] }}</p>
+          <v-card
+            v-for="(item, itemKey) in FxData"
+            class="mx-auto my-4 px-4"
+            max-width="344"
+            outlined
+            :key="itemKey"
+          >
+            <v-row align="center">
+              <v-col class="display-3" cols="6">
+                <p class="close_text">{{ item["4. close"] }}</p>
+              </v-col>
+              <v-col class="display-3" cols="6">
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <v-list-item-subtitle
+                      >open: {{ item["1. open"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >high: {{ item["2. high"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >low: {{ item["3. low"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >close: {{ item["4. close"] }}</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+          </v-card>
         </v-col>
-        <v-col class="display-3" cols="6">
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-subtitle
-                >open: {{ item["1. open"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >high: {{ item["2. high"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >low: {{ item["3. low"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >close: {{ item["4. close"] }}</v-list-item-subtitle
-              >
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-      </v-row>
-    </v-card>
-      </v-col>
         <v-col>
-            <highcharts :options="chartOptions"></highcharts>
+          <highcharts :options="chartOptions"></highcharts>
 
           <!-- <v-card class="pa-2" outlined tile> </v-card> -->
         </v-col>
@@ -47,31 +46,17 @@
   </div>
 </template>
 <script>
-import {Chart} from 'highcharts-vue'
+import { Chart } from "highcharts-vue";
 
 // import axios from "axios";
 export default {
   name: "Fx",
   data: function() {
     return {
-      duration: "Time Series FX (1min)",
-      // results: []
-      //    chartOptions: {
-      //   chart: {
-      //     type: 'spline'
-      //   },
-      //   title: {
-      //     text:  "FX Time Series (1min)"
-      //   },
-      //   series: [{
-      //     data: [10, 0, 8, 2, 6, 4, 5, 5]
-      //   }]
-      // },
+      duration: "Stocks time Series FX (1min)"
     };
   },
-    methods:{
-
-  },
+  methods: {},
   created() {
     this.$store.dispatch("fetchFx");
   },
@@ -79,25 +64,32 @@ export default {
     FxData() {
       return this.$store.state.FxData;
     },
-     chartOptions() {
-      const data = Object.entries(this.FxData).map(i => [i[0] ,parseFloat( i[1]['4. close'] )])
-      console.log('data', data)
+    chartOptions() {
+      const data = Object.entries(this.FxData).map(i => [
+        i[0],
+        parseFloat(i[1]["4. close"])
+      ]);
+      const categories = Object.keys(this.FxData).map(i => i.split(" ")[1]);
       return {
-
         chart: {
-          type: 'spline'
+          type: "spline"
+        },
+        xAxis: {
+          categories
         },
         title: {
           text: this.duration
         },
-        series: [{
-          data
-        }]
-      }
-      },
+        series: [
+          {
+            data
+          }
+        ]
+      };
+    }
   },
 
-   components: {
+  components: {
     highcharts: Chart
   }
 };
--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -2,48 +2,42 @@
   <div>
     <h1>{{ this.duration }}</h1>
     <v-container class="grey lighten-5">
-      <v-row >
+      <v-row>
         <v-col class="wrapper">
-
-
-
-
-
-    <v-card
-      v-for="(item, itemKey) in stocksData"
-      class="mx-auto my-4 px-4"
-      max-width="344"
-      outlined
-      :key="itemKey"
-    >
-      <v-row align="center">
-        <v-col class="display-3" cols="6">
-          <p class="close_text">{{ item["4. close"] }}</p>
-        </v-col>
-        <v-col class="display-3" cols="6">
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-subtitle
-                >open: {{ item["1. open"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >high: {{ item["2. high"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >low: {{ item["3. low"] }}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >close: {{ item["4. close"] }}</v-list-item-subtitle
-              >
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-      </v-row>
-    </v-card>
-
+          <v-card
+            v-for="(item, itemKey) in stocksData"
+            class="mx-auto my-4 px-4"
+            max-width="344"
+            outlined
+            :key="itemKey"
+          >
+            <v-row align="center">
+              <v-col class="display-3" cols="6">
+                <p class="close_text">{{ item["4. close"] }}</p>
+              </v-col>
+              <v-col class="display-3" cols="6">
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <v-list-item-subtitle
+                      >open: {{ item["1. open"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >high: {{ item["2. high"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >low: {{ item["3. low"] }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >close: {{ item["4. close"] }}</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+          </v-card>
         </v-col>
         <v-col>
-            <highcharts :options="chartOptions"></highcharts>
+          <highcharts :options="chartOptions"></highcharts>
           <!-- <v-card class="pa-2" outlined tile> </v-card> -->
         </v-col>
       </v-row>
@@ -52,13 +46,13 @@
 </template>
 
 <script>
-import {Chart} from 'highcharts-vue';
+import { Chart } from "highcharts-vue";
 
 export default {
   name: "Stocks",
   data: function() {
     return {
-      duration: "Time Series (1min)",
+      duration: "Time Series (1min)"
       // chartOptions: {
       //   chart: {
       //     type: 'spline'
@@ -79,25 +73,38 @@ export default {
     stocksData() {
       return this.$store.state.stocksData;
     },
-    getValues(){
-      console.log('Object.entries(this.stocksData)', Object.entries(this.stocksData))
-     return  Object.entries(this.stocksData)
+    getValues() {
+      console.log(
+        "Object.entries(this.stocksData)",
+        Object.entries(this.stocksData)
+      );
+      return Object.entries(this.stocksData);
     },
     chartOptions() {
-      const data = Object.entries(this.stocksData).map(i => [i[0] ,parseInt( i[1]['4. close'] , 10)])
-      return {
+      const data = Object.entries(this.stocksData).map(i => [
+        i[0],
+        parseInt(i[1]["4. close"], 10)
+      ]);
+      const categories = Object.keys(this.stocksData).map(i => i.split(" ")[1]);
 
+      return {
         chart: {
-          type: 'spline'
+          type: "spline"
+        },
+        xAxis: {
+          categories,
+          label : 'fx'
         },
         title: {
           text: this.duration
         },
-        series: [{
-          data
-        }]
-      }
-      },
+        series: [
+          {
+            data
+          }
+        ]
+      };
+    }
   },
   components: {
     highcharts: Chart
@@ -108,8 +115,8 @@ export default {
 .close_text {
   font-size: 32px;
 }
-.wrapper{
-      max-height: 100vh;
-    overflow-y: scroll;
+.wrapper {
+  max-height: 100vh;
+  overflow-y: scroll;
 }
 </style>
